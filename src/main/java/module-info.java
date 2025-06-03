2d65adaa07a5df6a5bc812439c5d9e5471906f09
nboyce <boycenobukho@gmail.com>
2025-06-03 22:47:34 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.learn_javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.learn_javafx to javafx.fxml;
+    exports org.example.learn_javafx;
+}
