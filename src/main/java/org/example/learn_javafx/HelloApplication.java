26af122d5fb4839618baf2e4d9dbb2a5aee5f92b
nboyce <boycenobukho@gmail.com>
2025-06-03 23:14:27 +0200

    hello world in javafx

--- src/main/java/org/example/learn_javafx/HelloApplication.java
+++ src/main/java/org/example/learn_javafx/HelloApplication.java
@@ -2,7 +2,11 @@ package org.example.learn_javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +14,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 600, Color.MEDIUMPURPLE); //A scene is a drawing surface for content and a container for nodes
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Greeting");
+        Text text = new Text();
+        text.setText("Hello, World!");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Verdana", 50));
+        text.setFill(Color.PURPLE);
+
+        root.getChildren().add(text);
         stage.setScene(scene);
         stage.show();
     }
