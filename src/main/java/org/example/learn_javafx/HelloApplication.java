ec02b58b15378ef4784338aef1438dee71f5b4e1
nboyce <boycenobukho@gmail.com>
2025-06-04 22:58:27 +0200

    creating a scene using scene builder and fxmlloader class

--- src/main/java/org/example/learn_javafx/HelloApplication.java
+++ src/main/java/org/example/learn_javafx/HelloApplication.java
@@ -16,27 +16,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 600, 600, Color.MEDIUMPURPLE); //A scene is a drawing surface for content and a container for nodes
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 600, 600, Color.MEDIUMPURPLE); //A scene is a drawing surface for content and a container for nodes
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Greeting");
-        Text text = new Text();
-        text.setText("Hello, World!");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.PURPLE);
 
-        Line line = new Line();//creating a horizontal line
-        line.setStartX(0);
-        line.setStartY(100);
-        line.setEndX(600);
-        line.setEndY(100);
-
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
         stage.setScene(scene);
         stage.show();
     }
