786b17020cb624164d0207165c52aabd359205d4
nboyce <boycenobukho@gmail.com>
2025-06-04 23:20:31 +0200

    functionality for clicking on the buttons

--- src/main/java/org/example/learn_javafx/HelloController.java
+++ src/main/java/org/example/learn_javafx/HelloController.java
@@ -1,5 +1,6 @@
 package org.example.learn_javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,18 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onUpClick(ActionEvent e){
+        System.out.println("UP");
+    }
+    public void onLeftClick(ActionEvent e){
+        System.out.println("LEFT");
+    }
+    public void onDownClick(ActionEvent e){
+        System.out.println("DOWN");
+    }
+    public void onRightpClick(ActionEvent e){
+        System.out.println("RIGHT");
     }
 }
