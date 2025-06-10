108b85890ea4120c9e691453f4880a3b797d67d0
nboyce <boycenobukho@gmail.com>
2025-06-10 21:11:13 +0200

    added methods for click events

--- src/main/java/org/example/learn_javafx/SceneController.java
+++ src/main/java/org/example/learn_javafx/SceneController.java
@@ -1,5 +1,32 @@
 package org.example.learn_javafx;
 
-public class SceneController{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class SceneController{
+    Parent root;
+    Stage stage;
+    Scene scene;
+
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene-2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene-1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
 }
