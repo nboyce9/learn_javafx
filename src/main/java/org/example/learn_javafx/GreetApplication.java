25eabb45001025ced5fccad7586730858d711b20
nboyce <boycenobukho@gmail.com>
2025-06-10 21:30:40 +0200

    content for linking the scene

--- src/main/java/org/example/learn_javafx/GreetApplication.java
+++ src/main/java/org/example/learn_javafx/GreetApplication.java
@@ -1,11 +1,18 @@
 package org.example.learn_javafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GreetApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Parent root = FXMLLoader.load(this.getClass().getResource("greet-input.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/org/example/learn_javafx/GreetController.java
+++ src/main/java/org/example/learn_javafx/GreetController.java
@@ -1,4 +1,5 @@
 package org.example.learn_javafx;
 
 public class GreetController {
+
 }
