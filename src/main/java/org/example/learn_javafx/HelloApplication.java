d0184e938a808ed566b78d959bffa45724248f4b
nboyce <boycenobukho@gmail.com>
2025-06-10 19:33:01 +0200

    clean up

--- src/main/java/org/example/learn_javafx/HelloApplication.java
+++ src/main/java/org/example/learn_javafx/HelloApplication.java
@@ -2,12 +2,7 @@ package org.example.learn_javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,8 +11,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-//        Group root = new Group();
-//        Scene scene = new Scene(root, 600, 600, Color.MEDIUMPURPLE); //A scene is a drawing surface for content and a container for nodes
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
