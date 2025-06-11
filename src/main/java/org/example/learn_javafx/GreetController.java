29469081a29b596edfb54e95073a34b7fbbeee41
nboyce <boycenobukho@gmail.com>
2025-06-11 23:29:50 +0200

    added a logout method

--- src/main/java/org/example/learn_javafx/GreetController.java
+++ src/main/java/org/example/learn_javafx/GreetController.java
@@ -1,14 +1,32 @@
 package org.example.learn_javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class GreetController {
 
     @FXML
     Label greetingLabel;
+    @FXML
+    AnchorPane scenePane;
+    Stage stage;
 
     public void greet(String name){
       greetingLabel.setText("Hello, " + name + ". Welcome!!!");
     }
+
+    public void logout(ActionEvent e){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want save yor work before exiting?: ");
+
+
+    }
+
 }
