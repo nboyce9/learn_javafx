b6fc107daff106d458da68ffe65a311ec61c5e35
nboyce <boycenobukho@gmail.com>
2025-06-11 23:41:42 +0200

    logout method to be called when closing the window using the x button

--- src/main/java/org/example/learn_javafx/GreetApplication.java
+++ src/main/java/org/example/learn_javafx/GreetApplication.java
@@ -1,9 +1,12 @@
 package org.example.learn_javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class GreetApplication extends Application {
@@ -14,5 +17,17 @@ public class GreetApplication extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void logout(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want save yor work before exiting?: ");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("logged out");
+            stage.close();
+        }
     }
 }
