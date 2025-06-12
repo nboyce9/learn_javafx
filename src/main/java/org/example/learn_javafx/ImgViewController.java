4e1f03ceb794b5082b792662914d22c07d0cf91c
nboyce <boycenobukho@gmail.com>
2025-06-12 23:28:47 +0200

    methodto displaty a diff imge when the button is clicked

--- src/main/java/org/example/learn_javafx/ImgViewController.java
+++ src/main/java/org/example/learn_javafx/ImgViewController.java
@@ -1,4 +1,20 @@
 package org.example.learn_javafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ImgViewController {
+
+    @FXML
+    ImageView imgView;
+    Button switchBtn;
+
+    Image img = new Image(getClass().getResourceAsStream("pinksmoke.jpg"));
+
+    public void displayImg(){
+        imgView.setImage(img);
+    }
+
 }
