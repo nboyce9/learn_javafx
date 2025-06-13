d21c9e1f55c3ceafcb794bed1239fd270d7c0896
nboyce <boycenobukho@gmail.com>
2025-06-13 16:37:30 +0200

    setting the stage

--- src/main/java/org/example/learn_javafx/SignupApplication.java
+++ src/main/java/org/example/learn_javafx/SignupApplication.java
@@ -1,4 +1,16 @@
 package org.example.learn_javafx;
 
-public class SignupApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SignupApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sign-up.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
